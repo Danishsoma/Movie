aedb4e42c8d3cb834a0052d6e266b9d02f15d021
Danish Soma <danishsoma113@gmail.com>
2023-02-20 19:14:08 +0530

    added initial css

--- src/Movie.js
+++ src/Movie.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useGlobalContext } from "./context";
 import { Link } from "react-router-dom";
+import './App.css'
 
 const defaultImage = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwZ4mTuUvdD6l60AzmWTIZ341ALx1udRQn3zv5va8czuI5VNApMbGqiIJGSuoe1EhreQY&usqp=CAU'
 
@@ -15,14 +16,15 @@ const Movie = () => {
     <div className="container">
        {data && data.map((item) => {
         const { imdbID: id, Poster: poster, Title: title, Year: year } = item;
-
         return (
+            <div className="item">
+                
             <Link to={`/movies/${id}`} key={id} className='movie'>
-                <h1>{title}</h1>
-                <h4>{year}</h4>
-                <img src={poster === 'N/A' ? defaultImage : poster } alt={title} />
+                <h1 className="heading">{title}</h1>
+                <h4 className="year">Year - {year}</h4>
+                <img className="card-image" src={poster === 'N/A' ? defaultImage : poster } alt={title} />
             </Link>
-
+            </div>
         )
        })}
     </div>
